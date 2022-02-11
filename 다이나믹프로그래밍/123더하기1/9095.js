2bb4907fadf585a7ddc03938ea1ea45991a9c824
wong0220 <dlsdnd0220@naver.com>
2022-02-11 19:30:15 +0900

    백준 다이나믹 프로그래밍 123더하기1 풀이

---
+++ 다이나믹프로그래밍/123더하기1/9095.js
@@ -0,0 +1,26 @@
+let fs = require("fs");
+let input = fs.readFileSync("예제.txt").toString().trim().split("\n");
+
+let N = input.shift();
+
+let newArr = input.map((el) => +el);
+
+function solution(N, newArr) {
+  let result = [];
+  for (let i = 0; i < N; i++) {
+    let dp = new Array(newArr[i]).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+
+    for (let j = 4; j <= newArr[i]; j++) {
+      dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+    }
+
+    result.push(dp[newArr[i]]);
+  }
+
+  console.log(result.join("\n"));
+}
+
+solution(N, newArr);
