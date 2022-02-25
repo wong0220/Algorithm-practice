56cc3727c06d77e275226fc87665f2ea6393d3e4
wong0220 <dlsdnd0220@naver.com>
2022-02-25 20:07:07 +0900

    프로그래머스 정렬 1단계 문제풀이

---
+++ 프로그래머스/정렬/1단계.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+  var answer = [];
+  for (let i = 0; i < commands.length; i++) {
+    let sortNum = array.slice(commands[i][0] - 1, commands[i][1]);
+
+    sortNum.sort((a, b) => a - b);
+    answer.push(sortNum[commands[i][2] - 1]);
+  }
+  return answer;
+}
